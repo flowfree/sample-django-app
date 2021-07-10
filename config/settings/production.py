d15022ea46318b5e7c2d21f467ad5cc03205f413
Nash <nashruddin.amin@gmail.com>
2021-07-10 12:34:43 +0700

    Added postgresql for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,3 +4,14 @@ from .base import *
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', 5432),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+    }
+}
